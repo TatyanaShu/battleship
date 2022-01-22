24f07f0d5d2fca1ec4176d0633b11ec60f77b99f
Tatyana Shuniborova <tashu529@gmail.com>
2022-01-22 23:38:13 +0300

    feat: add ship object

--- assets/js/battleship.js
+++ assets/js/battleship.js
@@ -1,46 +1,65 @@
-//start  game
-document.onload = function () {
-    let board = document.getElementById("board");
-    console.log(board);
-    // function myGame() {
-    //     board.insertAdjacentHTML("beforeend", "<div id="chooseGame" class="gameParametr">
-    //     <button class="start">Start</button>
-    //   </div>")
-    // }
-    // myGame()
-}
-//ship location
-//start battle
-//game circle
-//game over
 
-let randomLoc = Math.floor(Math.random() * 5),
-    location1 = randomLoc,
-    location2 = location1 + 1,
-    location3 = location2 + 1,
-    guess,
-    hits = 0,
-    guesses = 0,
-    isSunk = false;
-    
-while (isSunk == false) {
-    guess = prompt("Введите координаты выстрела (от 0 до 6)");
-    if (guess < 0 || guess > 6) {
-        alert("Введите корректное значение!"); 
-    } else {
-        guesses++;
-        if (location1 == guess || guess == location2 || guess == location3) {
-            alert("Попали!");
-            hits++;
-            if (hits == 3) {
-                isSunk = true;
-                alert("Вы выйграли!");
-            } 
-        } else {
-                alert("Вы промахнулись!");
-        }
+let view = {
+    displayMessage: function (msg) {
+        let messageArea = document.getElementById('message-area');
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    }, 
+    displayMiss: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "miss")
     }
 }
-let stats = "Вам понадобилось " + guesses + " ходов";
-alert(stats);
+// coordination  of ships
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3, shipsSunk: 0,
+    ships =[{
+        location: ['10', '20', '30'],
+        hits: ['', '', '']
+    },
+    {
+        location: ['32', '33', '33'],
+        hits: ['', '', '']
+    },
+    {
+        location: ['63', '64', '65'],
+        hits: ['', '', '']
+    }],
+    fire: function (guess) {
+        for (let i = 0; i<this.numShips; i++)
+    }
+}
+// let randomLoc = Math.floor(Math.random() * 5),
+//     location1 = randomLoc,
+//     location2 = location1 + 1,
+//     location3 = location2 + 1,
+//     guess,
+//     hits = 0,
+//     guesses = 0,
+//     isSunk = false;
     
+// while (isSunk == false) {
+//     guess = prompt("Введите координаты выстрела (от 0 до 6)");
+//     if (guess < 0 || guess > 6) {
+//         alert("Введите корректное значение!"); 
+//     } else {
+//         guesses++;
+//         if (location1 == guess || guess == location2 || guess == location3) {
+//             alert("Попали!");
+//             hits++;
+//             if (hits == 3) {
+//                 isSunk = true;
+//                 alert("Вы выйграли!");
+//             } 
+//         } else {
+//                 alert("Вы промахнулись!");
+//         }
+//     }
+// }
+// let stats = "Вам понадобилось " + guesses + " ходов";
+// alert(stats);
